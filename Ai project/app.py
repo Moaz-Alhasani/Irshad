535376633fefc11d32116507b3c651aff13706d4
Moaz-Alhasani <moazalhosne9@gmail.com>
2026-01-14 17:23:03 +0300

    correct

--- Ai project/app.py
+++ Ai project/app.py
@@ -443,6 +443,6 @@ def predict_acceptance():
         "matched_skills": matched
     })
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host="0.0.0.0", port=port, debug=False)
